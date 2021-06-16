e35f7e45af23c3b3411c8c75e4f9c55496e44ffa
Henrik VT <22358337+henrikvtcodes@users.noreply.github.com>
2021-06-15 23:42:54 -0400

    styling; tailwind colors, css @ rules, remove css

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,9 +5,29 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      blue: {
+        subtle: '#d1e1ef',
+        lightest: '#bdd4e6',
+        lighter: '#a6c5dd',
+        light: '#81acce',
+        DEFAULT: '#6f9abc',
+        dark: '#4f7898',
+        darker: '#355269',
+        darkest: '#283c4c'
+      },
+      gray: {
+        darkest: '#2C2F33',
+        dark: '#23272A',
+        DEFAULT: '#9CA3AF',
+      }
+    }
   },
   variants: {
     extend: {},
   },
   plugins: [],
 }
+
